263e76cb50f3a642e86456123381d0eab815cb0c
wanputera <wanputera17@gmail.com>
2024-01-25 00:48:13 +0800

    login page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LifeFlow-Hub</title>
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
